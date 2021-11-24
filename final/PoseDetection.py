d2e9937100907893e3e68127a8147a1a8a63aa01
giova86 <giovanni.bocchi@gmail.com>
2021-11-24 09:17:58 +0100

    test

--- final/PoseDetection.py
+++ final/PoseDetection.py
@@ -4,7 +4,7 @@ import time
 import mediapipe as mp
 import numpy as np
 import os
-from utils import mediapipe_detection, draw_landmarks, draw_landmarks_custom
+from utils import mediapipe_detection, draw_landmarks, draw_landmarks_custom, draw_limit_rh, draw_limit_lh, check_detection
 
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
@@ -13,20 +13,17 @@ cap = cv2.VideoCapture(0)
 with mp_holistic.Holistic(min_detection_confidence=0.5,
                           min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
-        black = np.zeros((1080, 1920, 3))
         ret, frame = cap.read()
-        #frame = cv2.flip(frame,1)
 
         # make detection
         image, results = mediapipe_detection(frame, holistic)
-        points_rh_x = []
-        points_rh_y = []
-        if results.right_hand_landmarks:
-            for i in range(21):
-                points_rh_x.append(results.right_hand_landmarks.landmark[i].x)
-                points_rh_y.append(results.right_hand_landmarks.landmark[i].y)
+
+        draw_limit_rh(frame, results)
+        draw_limit_lh(frame, results)
 
         draw_landmarks_custom(frame, results)
+
+        check_detection(frame, results)
 
         cv2.imshow('Face, Hands and Pose detection', frame)
 
--- final/utils.py
+++ final/utils.py
@@ -26,21 +26,82 @@ def draw_landmarks(image, results):
 
 def draw_landmarks_custom(image, results):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                             mp_drawing.DrawingSpec(color=(0,0,255),thickness=1, circle_radius=1),
-                             mp_drawing.DrawingSpec(color=(0,0,255),thickness=1, circle_radius=1),
+                             mp_drawing.DrawingSpec(color=(255,255,255),thickness=1, circle_radius=1),
+                             mp_drawing.DrawingSpec(color=(255,255,255),thickness=1, circle_radius=1),
                              )
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
                              mp_drawing.DrawingSpec(color=(80,110,10),thickness=1, circle_radius=1),
                              mp_drawing.DrawingSpec(color=(80,256,121),thickness=1, circle_radius=1),
                              )
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                             mp_drawing.DrawingSpec(color=(255,0,0),thickness=3, circle_radius=5),
-                             mp_drawing.DrawingSpec(color=(255,0,0),thickness=3, circle_radius=5),
+                             mp_drawing.DrawingSpec(color=(0,0,255),thickness=3, circle_radius=5),
+                             mp_drawing.DrawingSpec(color=(0,0,255),thickness=3, circle_radius=5),
                              )
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
                              mp_drawing.DrawingSpec(color=(255,0,0),thickness=3, circle_radius=5),
                              mp_drawing.DrawingSpec(color=(255,0,0),thickness=3, circle_radius=5),
                              )
+    #cv2.rectangle(image, start_point, end_point, color, thickness)
+
+def draw_limit_rh(image, results):
+    if results.right_hand_landmarks:
+        xMax = max([i.x for i in results.right_hand_landmarks.landmark])
+        xMin = min([i.x for i in results.right_hand_landmarks.landmark])
+        yMax = max([i.y for i in results.right_hand_landmarks.landmark])
+        yMin = min([i.y for i in results.right_hand_landmarks.landmark])
+        h,w,_ = image.shape
+        cv2.rectangle(image, (int(xMin*w), int(yMin*h)),  (int(xMax*w), int(yMax*h)), (255,0,0), 1)
+        cv2.line(image, (int(xMin*w), int(yMin*h)), (int(xMin*w), int(yMin*h)+int((yMax*h-yMin*h)/5)), (255,0,0),8)
+        cv2.line(image, (int(xMin*w), int(yMin*h)), (int(xMin*w)+int((xMax*w-xMin*w)/5), int(yMin*h)), (255,0,0),8)
+        cv2.line(image, (int(xMax*w), int(yMax*h)), (int(xMax*w), int(yMax*h)-int((yMax*h-yMin*h)/5)), (255,0,0),8)
+        cv2.line(image, (int(xMax*w), int(yMax*h)), (int(xMax*w)-int((xMax*w-xMin*w)/5), int(yMax*h)), (255,0,0),8)
+        cv2.line(image, (int(xMin*w), int(yMax*h)), (int(xMin*w), int(yMax*h)-int((yMax*h-yMin*h)/5)), (255,0,0),8)
+        cv2.line(image, (int(xMin*w), int(yMax*h)), (int(xMin*w)+int((xMax*w-xMin*w)/5), int(yMax*h)), (255,0,0),8)
+        cv2.line(image, (int(xMax*w), int(yMin*h)), (int(xMax*w), int(yMin*h)+int((yMax*h-yMin*h)/5)), (255,0,0),8)
+        cv2.line(image, (int(xMax*w), int(yMin*h)), (int(xMax*w)-int((xMax*w-xMin*w)/5), int(yMin*h)), (255,0,0),8)
+        cv2.putText(image, 'Right Hand',(int(xMin*w), int(yMin*h-(yMax*h-yMin*h)/20)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+
+def draw_limit_lh(image, results):
+    if results.left_hand_landmarks:
+        xMax = max([i.x for i in results.left_hand_landmarks.landmark])
+        xMin = min([i.x for i in results.left_hand_landmarks.landmark])
+        yMax = max([i.y for i in results.left_hand_landmarks.landmark])
+        yMin = min([i.y for i in results.left_hand_landmarks.landmark])
+        h,w,_ = image.shape
+        cv2.rectangle(image, (int(xMin*w), int(yMin*h)),  (int(xMax*w), int(yMax*h)), (0,0,255), 1)
+        cv2.line(image, (int(xMin*w), int(yMin*h)), (int(xMin*w), int(yMin*h)+int((yMax*h-yMin*h)/5)), (0,0,255),8)
+        cv2.line(image, (int(xMin*w), int(yMin*h)), (int(xMin*w)+int((xMax*w-xMin*w)/5), int(yMin*h)), (0,0,255),8)
+        cv2.line(image, (int(xMax*w), int(yMax*h)), (int(xMax*w), int(yMax*h)-int((yMax*h-yMin*h)/5)), (0,0,255),8)
+        cv2.line(image, (int(xMax*w), int(yMax*h)), (int(xMax*w)-int((xMax*w-xMin*w)/5), int(yMax*h)), (0,0,255),8)
+        cv2.line(image, (int(xMin*w), int(yMax*h)), (int(xMin*w), int(yMax*h)-int((yMax*h-yMin*h)/5)), (0,0,255),8)
+        cv2.line(image, (int(xMin*w), int(yMax*h)), (int(xMin*w)+int((xMax*w-xMin*w)/5), int(yMax*h)), (0,0,255),8)
+        cv2.line(image, (int(xMax*w), int(yMin*h)), (int(xMax*w), int(yMin*h)+int((yMax*h-yMin*h)/5)), (0,0,255),8)
+        cv2.line(image, (int(xMax*w), int(yMin*h)), (int(xMax*w)-int((xMax*w-xMin*w)/5), int(yMin*h)), (0,0,255),8)
+        cv2.putText(image, 'Left Hand',(int(xMin*w), int(yMin*h-(yMax*h-yMin*h)/20)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+
+def check_detection(image, results):
+    if results.left_hand_landmarks:
+        cv2.putText(image, 'Left Hand: DETECTED',(10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+    else:
+        cv2.putText(image, 'Left Hand: NOT DETECTED',(10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
+
+    if results.right_hand_landmarks:
+        cv2.putText(image, 'Right Hand: DETECTED',(10,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+    else:
+        cv2.putText(image, 'Right Hand: NOT DETECTED',(10,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+
+    if results.face_landmarks:
+        cv2.putText(image, 'Face: DETECTED',(10,110), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+    else:
+        cv2.putText(image, 'Face: NOT DETECTED',(10,110), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+
+    if results.face_landmarks:
+        cv2.putText(image, 'Pose: DETECTED',(10,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (80,256,121), 2)
+    else:
+        cv2.putText(image, 'Pose: NOT DETECTED',(10,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (80,256,121), 2)
+
+    # if results.face_landmarks:
+    #     cv2.putText(image, 'Right Hand',(int(xMin*w), int(yMin*h-(yMax*h-yMin*h)/20)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
 
 def points_detection(results):
     xMax = max([i.x for i in results.right_hand_landmarks.landmark])
