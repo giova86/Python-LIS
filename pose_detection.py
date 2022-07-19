58a93788780321ab6d0911e39bde185eafbe350c
Giovanni Bocchi <giovanni.bocchi@kiwidatascience.com>
2022-07-19 16:32:51 +0200

    code updates

--- pose_detection.py
+++ pose_detection.py
@@ -9,7 +9,7 @@ from utils import mediapipe_detection, draw_landmarks, draw_landmarks_custom, dr
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 with mp_holistic.Holistic(min_detection_confidence=0.5,
                           min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
